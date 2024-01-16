377462bd1846fb2427ece89997c3f043c92df693
김민주 <69795199+ks00919@users.noreply.github.com>
2024-01-16 23:02:20 +0900

    feat: 백준 10172

---
+++ 자바/백준/Bronze/BJ_10172_개.java
@@ -0,0 +1,18 @@
+/**
+ * [B5] 백준 10172 개
+ * 메모리 : 11212KB
+ * 시간 : 72ms
+ * 코드 길이 : 286B
+ * 
+ * @author 김민주
+ * @see <a href="https://www.acmicpc.net/problem/10172">
+ */
+class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
