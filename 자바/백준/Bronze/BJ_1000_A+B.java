cb4e28d2474cdaafc3c260b1038fcf7da65668e0
김민주 <69795199+ks00919@users.noreply.github.com>
2024-01-16 20:16:44 +0900

    feat: [B5] 백준 1000

---
+++ 자바/백준/Bronze/BJ_1000_A+B.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * [B5] 백준 1000 A+B
+ * 메모리 : 11508KB
+ * 시간 : 80ms
+ * 코드 길이 : 667B
+ * 
+ * @author 김민주
+ * @see <a href="https://www.acmicpc.net/problem/1000">
+ */
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a + b);
+    }
+}
