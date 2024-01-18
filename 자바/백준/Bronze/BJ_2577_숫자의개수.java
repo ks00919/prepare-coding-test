a1482bc91b0657fef05fad52cada1149431a0065
김민주 <69795199+ks00919@users.noreply.github.com>
2024-01-18 15:17:24 +0900

    feat: 백준 2577

---
+++ 자바/백준/Bronze/BJ_2577_숫자의개수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * [B2] 백준 2577 숫자의 개수
+ * 메모리 : 11532KB
+ * 시간 : 72ms
+ * 코드 길이 : 655B
+ * 
+ * @author 김민주
+ * @see <a href="https://www.acmicpc.net/problem/2577">
+ */
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int result = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+		result *= Integer.parseInt(br.readLine());
+		
+		int[] count = new int[10];
+		String results = String.valueOf(result);
+		
+		int length = results.length();
+		for(int i = 0; i < length; i++) {
+			count[results.charAt(i) - '0']++;
+		}
+		
+		for (int i = 0; i < count.length; i++) {
+			System.out.println(count[i]);
+		}
+	}
+}
