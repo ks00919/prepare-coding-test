01f8224f2c67a75e2d7933e0610ede468f80f8e6
김민주 <69795199+ks00919@users.noreply.github.com>
2024-01-22 21:14:42 +0900

    feat: 백준 1152

---
+++ java/baekjoon/Bronze/BJ_1152_단어의개수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * [B2] 백준 1152 단어의 개수
+ * 메모리 : 20348KB
+ * 시간 : 180ms
+ * 코드 길이 : 417B
+ * 
+ * @author 김민주
+ * @see <a href="https://www.acmicpc.net/problem/1152">
+ */
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
+
+        // String.split()을 쓰면 틀리는 이유 - 공백만 들어올 경우 결과가 1이 나온다!
+    }
+}
