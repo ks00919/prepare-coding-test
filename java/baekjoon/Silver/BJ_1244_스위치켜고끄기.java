b437a1eaa052b421fb70e56ca108e215ad458fbd
김민주 <69795199+ks00919@users.noreply.github.com>
2024-01-30 23:57:07 +0900

    Update BJ_1244_스위치켜고끄기.java

--- java/baekjoon/Silver/BJ_1244_스위치켜고끄기.java
+++ java/baekjoon/Silver/BJ_1244_스위치켜고끄기.java
@@ -5,8 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * [S4] 백준 1244 스위치 켜고 끄기
+ * 메모리 : 11612KB
+ * 시간 : 76ms
+ * 코드 길이 : 1599B
+ * 
+ * @author 김민주
+ * @see <a href="https://www.acmicpc.net/problem/1244">
+ */
+class Main {
 
+	// 스위치 저장
 	static int[] switchs;
 
 	public static void main(String[] args) throws IOException {
@@ -16,6 +26,7 @@ public class Main {
 		switchs = new int[n];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		// 스위치 값 초기화
 		for (int i = 0; i < n; i++) {
 			switchs[i] = parseInt(st.nextToken());
 		}
@@ -23,42 +34,50 @@ public class Main {
 		int studentNumber = parseInt(br.readLine());
 		for (int i = 0; i < studentNumber; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
+			// 남학생이면 boy() 호출
 			if (st.nextToken().equals("1")) {
 				boy(parseInt(st.nextToken()));
 			} else {
+				// 여학생은 girl() 호출
 				girl(parseInt(st.nextToken()));
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
+		// 한 줄에 20개의 스위치 출력
 		for (int i = 0; i < n; i++) {
 			sb.append(switchs[i]).append(' ');
 			if ((i + 1) % 20 == 0) {
 				sb.append('\n');
 			}
 		}
+		// 결과 출력
 		System.out.println(sb);
 	}
 
+	// 배정받은 숫자 입력
 	public static void boy(int number) {
-		for (int i = 1; i < switchs.length; i *= number) {
-			switchs[i] = switchs[i] == 0 ? 1 : 0;
+		// number의 배수마다 스위치 상태 변경
+		for (int i = number; i <= switchs.length; i += number) {
+			switchs[i - 1] = switchs[i - 1] == 0 ? 1 : 0;
 		}
 	}
 
+	// 배정받은 숫자 입력
 	public static void girl(int number) {
-		int index = number;
-		int count = 1;
+		// 인덱스에 맞게 1 감소
+		number -= 1;
+		// 배정받은 숫자 부분 스위치 상태 변경
+		switchs[number] = switchs[number] == 0 ? 1 : 0;
+		// 배정받은 숫자의 스위치를 기준으로 대칭이 같으면 상태 변경
 		for (int i = 1; number - i >= 0 && number + i < switchs.length; i++) {
-			if (switchs[number + i] != switchs[number - i]) {
+			int left = number - i;
+			int right = number + i;
+			if (switchs[left] != switchs[right]) {
 				break;
 			}
-			index -= 1;
-			count += 2;
-		}
-
-		for (int i = index; i <= count; i++) {
-			switchs[i] = switchs[i] == 0 ? 1 : 0;
+			switchs[left] = switchs[left] == 0 ? 1 : 0;
+			switchs[right] = switchs[right] == 0 ? 1 : 0;
 		}
 	}
 }
