67f717149a3e3eb7e37bbd6dd3cc0cff1ee882d8
김민주 <69795199+ks00919@users.noreply.github.com>
2024-02-19 23:01:08 +0900

    Create BJ_11720_숫자의합.java

---
+++ java/baekjoon/Bronze/BJ_11720_숫자의합.java
@@ -0,0 +1,24 @@
+import static java.lang.Integer.*;
+
+import java.io.*;
+
+/**
+ * [B4] 백준 11720 숫자의 합
+ * 메모리 : 11528KB
+ * 시간 : 76ms
+ * 코드 길이 : 453B
+ */
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = parseInt(br.readLine());
+        String input = br.readLine();
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += input.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+
+}
