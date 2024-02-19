cdcb1745a1b312b857ff03b80dab8df8fa273ec4
김민주 <69795199+ks00919@users.noreply.github.com>
2024-02-19 22:56:17 +0900

    Create BJ_11654_아스키코드.java

---
+++ java/baekjoon/Bronze/BJ_11654_아스키코드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+/**
+ * [B5] 백준 11654 아스키 코드
+ * 메모리 : 11460KB
+ * 시간 : 80ms
+ * 코드 길이 : 253B
+ */
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int) br.readLine().charAt(0));
+    }
+
+}
