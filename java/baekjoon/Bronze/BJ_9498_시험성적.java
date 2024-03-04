ec27243753de93af2b15c0a73da38c710036673c
김민주 <69795199+ks00919@users.noreply.github.com>
2024-03-04 16:00:15 +0900

    Create BJ_9498_시험성적.java

---
+++ java/baekjoon/Bronze/BJ_9498_시험성적.java
@@ -0,0 +1,30 @@
+import static java.lang.Integer.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * [B5] 백준 9498 시험 성적
+ * 메모리 : 11520KB
+ * 시간 : 76ms
+ * 코드 길이 : 636B
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = parseInt(br.readLine());
+
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
