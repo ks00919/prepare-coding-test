6365296c2e8b927777d33c7e1fc8b0b2bf907fde
김민주 <69795199+ks00919@users.noreply.github.com>
2024-03-05 17:50:34 +0900

    Create BJ_9012_괄호.java

---
+++ java/baekjoon/Silver/BJ_9012_괄호.java
@@ -0,0 +1,41 @@
+import static java.lang.Integer.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * [S4] 백준 9012 괄호
+ * 메모리 : 11692KB
+ * 시간 : 84ms
+ * 코드 길이 : 918B
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            sb.append(validation(br.readLine().toCharArray()) ? "YES" : "NO").append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static boolean validation(char[] array) {
+        Stack<Character> s = new Stack<>();
+
+        int size = array.length;
+
+        for (int i = 0; i < size; i++) {
+            if (array[i] == '(')
+                s.add('(');
+            else {
+                if (s.isEmpty())
+                    return false;
+                s.pop();
+            }
+        }
+        return s.isEmpty();
+    }
+}
