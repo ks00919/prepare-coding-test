239edfcb7c55445471083c1bd075dcda66d9c805
Minju <69795199+ks00919@users.noreply.github.com>
2025-03-12 22:41:58 +0900

    [Bronze II] Title: 숫자, Time: 68 ms, Memory: 11704 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a == b) {
+            System.out.println(0);
+            System.exit(0);
+        } else if (a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(b - a - 1).append("\n");
+
+        for (int i = a + 1; i < b; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
