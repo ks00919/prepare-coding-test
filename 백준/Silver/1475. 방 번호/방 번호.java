7716bab72aa7c2d40c76f7450e1370168047f8dc
Minju <69795199+ks00919@users.noreply.github.com>
2025-03-13 23:34:27 +0900

    [Silver V] Title: 방 번호, Time: 188 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String roomNumber = br.readLine();
+        int length = roomNumber.length();
+
+        int[] counts = new int[9];
+
+        for (int i = 0; i < length; i++) {
+            if (roomNumber.charAt(i) == '9') counts[6]++;
+            else counts[roomNumber.charAt(i) - '0']++;
+        }
+        counts[6] = (counts[6] + 1) / 2;
+        System.out.println(Arrays.stream(counts).max().getAsInt());
+    }
+}
