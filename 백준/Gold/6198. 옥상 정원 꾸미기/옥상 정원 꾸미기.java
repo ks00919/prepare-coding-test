56bc2eb26aada16b9a638c963d8e08190f36d186
Minju <69795199+ks00919@users.noreply.github.com>
2025-05-20 17:36:54 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 276 ms, Memory: 23240 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int height = Integer.parseInt(br.readLine());
+            while (!stack.isEmpty() && stack.peek() <= height) {
+                stack.pop();
+            }
+            sum += stack.size();
+            stack.push(height);
+        }
+
+        System.out.println(sum);
+    }
+}
